c34cd2848a0d6fd76e176dea514d131cc2d40a83
fabiantoni0411-sketch <fabiantoni0411@gmail.com>
2026-07-26 03:48:34 -0300

    Add files via upload

--- app/baralho-cigano/page.tsx
+++ app/baralho-cigano/page.tsx
@@ -1,1 +1,11 @@
+import BaralhoCigano from "@/components/BaralhoCigano";
+import "@/components/baralho-cigano.css";
 
+export const metadata = {
+  title: "Mensagem do Baralho Cigano | Altar Virtual",
+  description: "Escolha três cartas do baralho cigano e receba sua mensagem.",
+};
+
+export default function PaginaBaralhoCigano() {
+  return <BaralhoCigano />;
+}
